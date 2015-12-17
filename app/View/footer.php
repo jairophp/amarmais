189f8e84f03b77b477f0dba92c5bd320cd5b9658
hp <henrdique_ddeus@hotmail.com>
2015-12-17 00:49:07 -0300

    novo footer.

--- app/View/footer.php
+++ app/View/footer.php
@@ -16,6 +16,113 @@
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <footer class="cempocento">
+    <div class="oitentapocento">
+        <section class="vinteporcento">
+            <i class="iconefooter fa fa-heartbeat fa-2x"></i>
+            <h6 class="titulofooter">Clinical Center</h6>
+            <h6 class="descricaofooter">Lorem Ipsum é simplesmente uma simulação de texto da indústria.</h6>
+            <ul>
+                <li class="contato">
+                    <i class="fa fa-building iconecontato"></i>
+                     PT, East 39th Street, New York, NY
+                </li>
+                <li class="contato">
+                    <i class="fa fa-whatsapp iconecontato"></i>
+                    +55 99 99999-9999
+                </li>
+                <li class="contato">
+                    <i class="fa fa-envelope iconecontato" ></i>
+                     contacto@doctor.com
+                </li>
+                <li class="contato">
+                    <i class="fa fa-tv iconecontato"></i>
+                    www.researchcenter.com
+                </li>
+            </ul>
+
+        </section>
+        <section class="vinteporcento">
+            <i class="iconefooter fa fa-heartbeat fa-2x"></i>
+            <h6 class="titulofooter">Recente News</h6>
+            
+            <section class="noticia">
+                <img class="imagemnoticia" src="img/logo.jpg">
+                <h6 class="noticiafooter">Lorem Ipsum é uma  simples simulação!</h6>
+                <h6 class="postagemnoticia">Post 15 April 2015</h6>
+            </section>
+            <section class="noticia">
+                <img class="imagemnoticia" src="img/logo.jpg">
+                <h6 class="noticiafooter">Lorem Ipsum é uma  simples simulação!</h6>
+                <h6 class="postagemnoticia">Post 15 April 2015</h6>
+            </section>
+        </section>
+        <section class="vinteporcento">
+            <i class="iconefooter fa fa-heartbeat fa-2x"></i>
+            <h6 class="titulofooter">Patient Information</h6>
+
+            <section class="noticia">
+                <ul>
+                    <li class="contato">
+                        <i class="fa fa-angle-right"></i>
+                        Ernst Jung Prize
+                    </li>
+                    <li class="contato">
+                        <i class="fa fa-angle-right"></i>
+                        William Allan Award
+                    </li>
+                    <li class="contato">
+                        <i class="fa fa-angle-right"></i>
+                        Ernst Jung Prize
+                    </li>
+                    <li class="contato">
+                        <i class="fa fa-angle-right"></i>
+                        William Allan Award
+                    </li>
+                    <li class="contato">
+                        <i class="fa fa-angle-right"></i>
+                        Ernst Jung Prize
+                    </li>
+                </ul>
+            </section>
+        </section>
+        <section class="vinteporcento">
+            <i class="iconefooter fa fa-heartbeat fa-2x"></i>
+            <h6 class="titulofooter">Our Image Gallery</h6>
+
+            <section class="noticia cempocento">
+                <img class="imagemgaleria" src="img/logo.jpg">
+                <img class="imagemgaleria" src="img/logo.jpg">
+                <img class="imagemgaleria" src="img/logo.jpg">
+
+                <img class="imagemgaleria" src="img/logo.jpg">
+                <img class="imagemgaleria" src="img/logo.jpg">
+                <img class="imagemgaleria" src="img/logo.jpg">
+            </section>
+        </section>
+    </div>
+
+    <div class="cempocento fundo">
+        <div class="oitentapocento fundo">
+            <h6 class="textofundo">Dotor Medical Clinic. Copyright 2018. All Reserved.</h6>
+            <section class="redesocial">
+                <div class="iconesocial">
+                    <i class="fa fa-skype is"></i>
+                </div>
+                <div class="iconesocial">
+                    <i class="fa fa-linkedin is"></i>
+                </div>
+                <div class="iconesocial">
+                    <i class="fa fa-pinterest is"></i>
+                </div>
+                <div class="iconesocial">
+                    <i class="fa fa-facebook is"></i>
+                </div>
+                <div class="iconesocial">
+                    <i class="fa fa-twitter is"></i>
+                </div>
+            </section>
+        </div>
+    </div>
 
 </footer>
 
